618a4dbabf1f10b188d6bc627d7112f8e2c3d66d
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-05-13 23:46:13 -0400

    Create Tables

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE employeesDB;
+
+
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(45) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(45) NULL, 
+    salary DECIMAL (10,3) NULL,
+    department_id INT NULL, 
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    
+)
